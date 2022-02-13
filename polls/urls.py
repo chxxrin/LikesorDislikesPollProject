ced460013b199f7324529c24dd912c7ca4b3dcd5
kjy1101 <amelia1023@naver.com>
2022-02-13 19:03:17 +0900

    결과화면 삭제, 그래프 수정

--- polls/urls.py
+++ polls/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
 
     path('vote/result', vote, name = "vote"),
 
-    path('vote_result/<int:type_pk>/<int:user_type>/<int:c1>/<int:c2>/<int:c3>/<int:c4>/<int:c5>/<int:c6>/<int:c7>/<int:c8>/<int:c9>/<int:c10>/', vote_result, name = "vote_result"),
- 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
